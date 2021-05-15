eff467f04f760b6fcbb88ce6e4156f701d494542
Simon Davis <simonfreelance1@gmail.com>
2021-05-15 16:36:23 -0400

    Added loading and error handling

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,59 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { HorizontalBars } from "../components/HorizontalBars";
 import useSWR from "swr";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { format } from "d3";
-const fetcher = (url) => fetch(url).then((res) => res.json());
+import axios from "axios";
+const Error = ({ children }) => {
+  return (
+    <div
+      style={{
+        background: "#1A1110",
+        color: "white",
+        padding: "1rem",
+        width: "100%",
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+const fetcher = (url) => axios.get(url).then((res) => res.data);
+
 export default function Home() {
-  const { data = [] } = useSWR("/api/population-by-country", fetcher);
+  const {
+    data = [],
+    isValidating,
+    revalidate,
+    error,
+  } = useSWR("/api/population-by-country", fetcher, {
+    refreshWhenOffline: true,
+    shouldRetryOnError: false,
+  });
+  const [dots, setDots] = useState(".");
+  useEffect(() => {
+    let updateLoadindicator;
+    if (isValidating) {
+      updateLoadindicator = setInterval(() => {
+        setDots((prevState) => {
+          if (prevState.length === 3) {
+            setDots("");
+          } else {
+            setDots(prevState + ".");
+          }
+        });
+      }, 500);
+    }
+    return () => {
+      clearInterval(updateLoadindicator);
+    };
+  }, [isValidating]);
   const [countriesToShow, setCountriesToShow] = useState(10);
   const [isAscending, setIsAscending] = useState(false);
   const formatPopulation = (value) => {
     const toNumber = value.replace(/,/g, "");
     return format(".2s")(toNumber).replace("G", "B");
   };
-
   const countriesData = data
     .slice(0, countriesToShow)
     .map((d) => {
@@ -118,6 +159,47 @@ export default function Home() {
           }`}
           </p>
         </div>
+        {isValidating && countriesData.length === 0 ? (
+          <div
+            style={{
+              margin: "12px 0",
+              fontSize: "2rem",
+            }}
+          >
+            Loading Data{dots}
+          </div>
+        ) : null}
+
+        {error?.response?.status === 429 ? (
+          <Error>
+            You have requested the countries' data way too many times. Come back
+            in an hour.
+          </Error>
+        ) : null}
+        {error?.response?.status !== 429 && error?.response?.status ? (
+          <div
+            style={{
+              display: "flex",
+              flexFlow: "column",
+              alignItems: "center",
+            }}
+          >
+            <Error>
+              An error has occured, try again later or make an issue in the
+              original github repo.
+            </Error>
+            <button
+              onClick={revalidate}
+              style={{
+                margin: "1rem",
+                fontSize: "1rem",
+                padding: "1em",
+              }}
+            >
+              Refetch?
+            </button>
+          </div>
+        ) : null}
         <HorizontalBars
           style={{
             margin: "20px",
